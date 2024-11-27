268e6b76ae3e0a953a58f60e94198621ba126f61
Sankar Paikira <shankarpaikira225@gmail.com>
2024-11-27 16:04:51 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session
+from flask_mysqldb import MySQL
 import subprocess
 import os
 
-app = Flask(__name__)
+app = Flask('__name__')
+app.secret_key = 'your-secret-key'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'login'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    if'username' in session:
+        return render_template('index.html', username=session['username'])
+    else:
+        return render_template('index.html')
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        pwd = request.form['password']
+        cur = mysql.connection.cursor()
+        cur.execute(f"select username, password from users where username = '{username}")
+        user = cur.fetchone()
+        cur.close()
+        if user and pwd == user[1]:
+            session['username'] = user[0]
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html', error='Invalid username or password')
+
+    return render_template('login.html')
 
 @app.route('/train_rf_model', methods=['POST'])
 def train_rf_model():
@@ -41,6 +72,9 @@ def check_training_status():
         return jsonify({'status': 'complete', 'accuracy': accuracy})
     else:
         return jsonify({'status': 'in_progress'})
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
